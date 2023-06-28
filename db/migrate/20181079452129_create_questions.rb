26d341019bbfa467b352b6a5d88ea847ca0135ff
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-06-29 00:49:45 +0700

    Update 20181079452129_create_questions.rb

--- db/migrate/20181079452129_create_questions.rb
+++ db/migrate/20181079452129_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :body
-      t.references :test
+      t.references :test, foreign_key: true
 
       t.timestamps
     end
