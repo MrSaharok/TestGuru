6b8bae59ba405951cfb77359995c15778b2134d2
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-06-29 16:24:11 +0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230627154549) do
+ActiveRecord::Schema.define(version: 20131079442121) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20230627154549) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "categories", force: :cascade do |t|
