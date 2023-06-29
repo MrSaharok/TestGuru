fd42b13de5c3e2d8f3ab8d83e229dacaf01643ac
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-06-29 12:49:45 +0700

    Update 20181552023942_create_answers.rb

--- db/migrate/20181552023942_create_answers.rb
+++ db/migrate/20181552023942_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.string :body, null: false
+      t.string :body
       t.boolean :correct, null: false
       t.references :question, foreign_key: true
 
