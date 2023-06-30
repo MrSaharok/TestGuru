4635f06cdc8f9a2b368a766127bfd312b216bef4
MrSaharok <zhelticov@gmail.com>
2023-07-01 01:57:54 +0700

    Добавил авторов

--- db/migrate/20230627154548_create_tests.rb
+++ db/migrate/20230627154548_create_tests.rb
@@ -2,9 +2,10 @@ class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level
+      t.integer :level, null: false
       t.references :category, foreign_key: true, null: false
-      
+      t.references :author, references: :user, null: false
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131079442121) do
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "category_id"
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_154549) do
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -29,22 +28,31 @@ ActiveRecord::Schema.define(version: 20131079442121) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id"
+    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table "answers", force: :cascade do |t|
-    t.text "body", null: false
-    t.boolean "correct", default: false, null: false
-    t.integer "question_id"
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", null: false
+    t.integer "category_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
