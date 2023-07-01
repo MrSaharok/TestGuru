eb2166e7bc7dcca4f9b192a7b6e416ebff5ac66e
MrSaharok <zhelticov@gmail.com>
2023-07-02 00:21:02 +0700

    Cod

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_154549) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
@@ -55,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_154549) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
