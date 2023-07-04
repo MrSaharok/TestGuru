a019994d94e6822760c3ad92e2256da7c2e4c482
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-07-04 12:37:44 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :test_passages
-  has_many :tests, through: :test_passages
+  has_many :test_passings
+  has_many :tests, through: :test_passings
 
   def test_by_level(level)
     tests.where(level: level)
