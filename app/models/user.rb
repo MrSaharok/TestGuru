44037cccc76927fb4d50809eadd2962ce9f2c1ba
MrSaharok <zhelticov@gmail.com>
2023-07-11 00:06:01 +0700

    Scope and validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   has_many :test_passings, dependent: :destroy
   has_many :tests, through: :test_passings, dependent: :destroy
 
+  validates :email, presence: true
+
+  scope :test_level, -> (level) { where(level: level) }
 
   def test_by_level(level)
     tests.where(level: level)
   end
 end
-
