b0eb4bf825d6d0cd0a4e713c960b3856c276811e
MrSaharok <zhelticov@gmail.com>
2023-08-11 22:06:02 +0700

    Fix

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,12 +1,2 @@
 module GistsHelper
-  def truncate(text, options = {}, &block)
-    if text
-      length = options.fetch(:length, 30)
-
-      content = text.truncate(length, options)
-      content = options[:escape] == false ? content.html_safe : ERB::Util.html_escape(content)
-      content << capture(&block) if block_given? && text.length > length
-      content
-    end
-  end
 end
