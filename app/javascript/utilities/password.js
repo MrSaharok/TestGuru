0fbafd16f5a3661b307d12005619379fd7fc1219
MrSaharok <zhelticov@gmail.com>
2023-08-13 22:00:07 +0700

    JS

---
+++ app/javascript/utilities/password.js
@@ -0,0 +1,38 @@
+document.addEventListener('turbolinks:load', () => {
+    const form = document.querySelector('#new_user')
+
+    if (form) {
+        new Password(form).listen()
+    }
+})
+
+class Password {
+
+    constructor(form) {
+        this.form = form
+        this.password = document.getElementById('password')
+        this.confirmation = document.getElementById('password-confirmation')
+        this.error = document.getElementById('confirmation-alert')
+    }
+
+    listen = () => {
+        this.form.addEventListener('input', this.passwordValidate.bind(this))
+    }
+
+    passwordValidate = () => {
+        if (!this.confirmation.value) {
+            this.error.textContent = ''
+            return
+        }
+
+        if (this.confirmation.value !== this.password.value) {
+            this.error.classList.remove('text-success')
+            this.error.classList.add('text-danger')
+            this.error.textContent = 'Passwords do not match'
+        } else {
+            this.error.classList.remove('text-danger')
+            this.error.classList.add('text-success')
+            this.error.textContent = 'Passwords match'
+        }
+    }
+}
