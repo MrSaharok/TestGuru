ff4c698f34493c7dbc423c0c377d7c07afed9211
MrSaharok <zhelticov@gmail.com>
2023-09-24 16:59:39 +0700

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ require("@popperjs/core")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import "controllers"
 import 'bootstrap'
 import { Tooltip, Popover } from "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -13,23 +13,20 @@ import '../utilities/password'
 import '../utilities/form_inline'
 import '../utilities/progressBar'
 
-// The stylesheet location we created earlier
-require("../stylesheets/application.css")
+require("../stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
 document.addEventListener("turbolinks:load", () => {
-    // Both of these are from the Bootstrap 5 docs
-    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    let tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
         return new Tooltip(tooltipTriggerEl)
     })
 
-    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    let popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
         return new Popover(popoverTriggerEl)
     })
 })
